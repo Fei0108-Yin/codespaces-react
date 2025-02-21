f609e82a98c3b0bc9fa6f31acf1ffb61ed9ca491
Fei0108-Yin <1605441084@qq.com>
2025-02-21 09:04:58 +0000

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/codespaces-react/' // 仓库名称，确保路径正确
+  base: '/codespaces-react/', // 设置 GitHub Pages 的路径
 });
