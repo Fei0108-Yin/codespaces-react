ad4ea8057f6adfc037ef3c3109afc983e965ac3e
Fei0108-Yin <1605441084@qq.com>
2025-02-21 08:41:02 +0000

    Restore src/App.jsx from commit 4b6db6e

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import SalesDashboard from "./SalesDashboard";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>React 前端示例</h1>
-      <SalesDashboard />
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
     </div>
   );
 }
